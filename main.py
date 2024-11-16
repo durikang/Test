a10f37ff6765c891f2056b34c2b5e78190a7ec99
duri <kooda21@naver.com>
2024-11-17 08:28:27 +0900

    하

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # main.py
+import logging
 import sys
 from PyQt5.QtWidgets import QApplication
 from GUI.mainWindow import MainWindow
 
+# 로깅 설정
+logging.basicConfig(filename='app_error.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        logging.exception("Unhandled exception occurred")  # 발생한 예외 기록
